deba5690aafcf9176ae4850569f0cc5f271a7931
n4s <s5935512004@phuket.psu.ac.th>
2019-09-12 07:55:44 +0700

    refactor to AsyncAwait

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,19 +3,18 @@ const ganache = require('ganache-cli')
 const Web3 = require('web3')
 const web3 = new Web3(ganache.provider())
 
-beforeEach(() => {
+let accounts
+
+beforeEach(async () => {
     // Get a list of all accounts
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-            console.log(fetchedAccounts)
-        })
-    
+    accounts = await web3.eth.getAccounts()
+
     // Use one of those accoints to deploy
     // the contracts
 })
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        
+        console.log(accounts)
     })
 })
